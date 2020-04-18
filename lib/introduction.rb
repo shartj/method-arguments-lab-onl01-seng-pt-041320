4afa42059a6123db8eb4013207184d3f99d967d4
Shari Jointe <stj0785@gmail.com>
2020-04-18 19:07:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,6 @@
 # lib/ introduction.RUBY_VERSION
 def say_hello_x_times (x)
   x.times do
+    
 end
 end
-def say_anything_x_times (phrase, x)
-  x.times do
-end
-end
-say_anything_x_times("Hello", 20)
