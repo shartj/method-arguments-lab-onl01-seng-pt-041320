412676f73f124f8366a5aed7eeb411cab9e721d0
Shari Jointe <stj0785@gmail.com>
2020-04-18 19:38:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ def introduction (x)
 
 end
 def introduction_with_language(name, language)
-puts "Hi, my name is Josh and I am learning to program in Ember.js."
+puts "Hi, my name is Dan and I am learning to program in Ember.js."
 end
